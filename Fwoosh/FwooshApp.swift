f40993367511ab1ea27da49d677f8049a00123fb
Andrzej Prochyra <andrzej@prochyra.io>
2025-08-14 20:02:44 +0100

    Initial Commit

---
+++ Fwoosh/FwooshApp.swift
@@ -0,0 +1,17 @@
+//
+//  FwooshApp.swift
+//  Fwoosh
+//
+//  Created by Andrzej Prochyra on 14/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct FwooshApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FwooshTests/FwooshTests.swift
@@ -0,0 +1,17 @@
+//
+//  FwooshTests.swift
+//  FwooshTests
+//
+//  Created by Andrzej Prochyra on 14/08/2025.
+//
+
+import Testing
+@testable import Fwoosh
+
+struct FwooshTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

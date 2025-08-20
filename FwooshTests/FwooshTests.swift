702ddc9989ba3048c8c025d664cc185ae2a29d40
Petar Simonovic <petar@simonovic.com>
2025-08-20 20:26:00 +0100

    build: add SwiftLint to build and test targets

--- FwooshTests/FwooshTests.swift
+++ FwooshTests/FwooshTests.swift
@@ -13,6 +13,7 @@ struct FwooshTests {
 
     @Test func example() async throws {
         // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        
     }
 
 }

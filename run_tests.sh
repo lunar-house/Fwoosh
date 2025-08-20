2f832624d879a57bacda1c27968b065ac90f3182
Petar Simonovic <petar@simonovic.com>
2025-08-20 21:23:53 +0100

    ci: reid-ify ios.yml

---
+++ run_tests.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+SCHEME="Fwoosh"
+DESTINATION="platform=iOS Simulator,OS=latest,name=iPhone 16"
+
+xcodebuild test -scheme "$SCHEME" -sdk iphonesimulator -destination "$DESTINATION" CODE_SIGNING_ALLOWED='NO'
+

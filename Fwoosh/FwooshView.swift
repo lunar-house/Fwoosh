80ed5b3aaad66d4160e3e0fd0a867ce817a0788d
Andrzej Prochyra <andrzej@prochyra.io>
2025-12-08 20:22:04 +0000

    refactor: rename view

--- Fwoosh/FwooshView.swift
+++ Fwoosh/FwooshView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct FwooshView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    FwooshView()
 }
